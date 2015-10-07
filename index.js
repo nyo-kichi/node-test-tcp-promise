59bd7d11bcad39d195edbdf6ad4419acfe4b16f7
nyo-kichi <nyookichi@gmail.com>
2015-10-07 22:59:18 +0900

    add emptyPort()

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
+let TestTCP = require('test-tcp');
 
 function testTcp() {}
+
+testTcp.emptyPort = (port) => {
+    return new Promise((resolve, reject) => {
+        TestTCP.empty_port(port, (err, gettingPort) => {
+            if (err) reject(err);
+            resolve(gettingPort);
+        });
+    });
+};
 
 module.exports = testTcp;
--- test/index.js
+++ test/index.js
@@ -8,4 +8,41 @@ describe('index', () => {
             assert(typeof testTcp === 'function');
         });
     });
+
+    describe('emptyPort()', () => {
+        it('should return Promise object', () => {
+            let got = testTcp.emptyPort();
+            assert(got instanceof Promise);
+            return got;
+        });
+
+        it('should get empty port', () => {
+            return testTcp.emptyPort().then(port => {
+                assert(port);
+            });
+        });
+
+        it('should get specified port', () => {
+            return testTcp.emptyPort(55555).then(port => {
+                assert(55555 <= port && port < 60000);
+            });
+        });
+    });
+
+    describe('emptyPort() error', () => {
+        let TestTCP = require('test-tcp'),
+            backup = TestTCP.empty_port;
+
+        afterEach(() => { TestTCP.empty_port = backup; });
+
+        it('should catch error if error occur', () => {
+            TestTCP.empty_port = (port, callback) => {
+                process.nextTick(() => { callback('empty port not found'); });
+            };
+
+            return testTcp.emptyPort().catch(err => {
+                assert(err === 'empty port not found');
+            });
+        });
+    });
 });
